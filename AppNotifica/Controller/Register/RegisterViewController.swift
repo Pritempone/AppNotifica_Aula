dc2784218c3422b7ea240dd83f3616a187ca9c0c
Pritempone <103539007+Pritempone@users.noreply.github.com>
2022-11-29 09:48:29 -0300

    Home e ViewControllerDefault

--- AppNotifica/Controller/Register/RegisterViewController.swift
+++ AppNotifica/Controller/Register/RegisterViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: ViewControllerDefault {
    
     //MARK: -  Clouseres
     var onLoginTap: (() -> Void)?
@@ -32,8 +32,6 @@ class RegisterViewController: UIViewController {
        override func viewDidLoad() {
            super.viewDidLoad()
         self.title = "Registrar"
-           
-           self.navigationController?.navigationBar.prefersLargeTitles=true
 
        }
 
